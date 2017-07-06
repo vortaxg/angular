7d0707a93734aa8567945074db2426339b41899d
Vitalii <vortaxg@gmail.com>
2017-07-06 13:31:09 +0300

    init demo-form-with-validations-explicit component

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -8,12 +8,14 @@ import {
 import { AppComponent } from './app.component';
 import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
 import { DemoFormSkuWithBuilderComponent } from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
+import { DemoFormWithValidationsExplicitComponent } from './demo-form-with-validations-explicit/demo-form-with-validations-explicit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoFormSkuComponent,
-    DemoFormSkuWithBuilderComponent
+    DemoFormSkuWithBuilderComponent,
+    DemoFormWithValidationsExplicitComponent
   ],
   imports: [
     BrowserModule,
---
+++ forms/src/app/demo-form-with-validations-explicit/demo-form-with-validations-explicit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-form-with-validations-explicit',
+  templateUrl: './demo-form-with-validations-explicit.component.html',
+  styleUrls: ['./demo-form-with-validations-explicit.component.css']
+})
+export class DemoFormWithValidationsExplicitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
